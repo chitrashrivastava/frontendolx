a59e7f0f0e828f98f1a7c855ffd4bc4910011850
chitrashrivastava <chitrashrivastava64@gmail.com>
2024-04-07 17:05:08 +0530

    addd link

--- src/component/config/axios.jsx
+++ src/component/config/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL:
-         "http://localhost:3003",
+         "https://backendolx.vercel.app/",
     withCredentials: true,
 });
 
